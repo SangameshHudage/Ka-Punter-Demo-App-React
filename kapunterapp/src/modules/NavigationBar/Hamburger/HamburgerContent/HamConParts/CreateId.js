b5110e6d7adf23955844650a4d33807e9f75700f
SangameshHudage <90179966+SangameshHudage@users.noreply.github.com>
2024-05-27 12:53:33 +0530

    Delete kapunterapp directory
    Adding updated file

--- kapunterapp/src/modules/NavigationBar/Hamburger/HamburgerContent/HamConParts/CreateId.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function CreateId() {
-  return (
-    <div>CreateId</div>
-  )
-}
-
-export default CreateId
--- kapunterapp/src/modules/NavigationBar/Hamburger/HamburgerContent/HamConParts/WithdrawalDetails.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function WithdrawalDetails() {
-  return (
-    <div>withdrawalDetails</div>
-  )
-}
-
-export default WithdrawalDetails
